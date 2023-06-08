f89ae4c7b5a5ec45465f204d4da0f60c9dab47d8
Jordan Kay <jordan@fleuronic.com>
2023-06-08 17:23:14 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+	name: "Parametric",
+	platforms: [
+		.iOS(.v16)
+	],
+	products: [
+		.library(
+			name: "Parametric",
+			targets: ["Parametric"]
+		)
+	],
+	dependencies: [.package(url: "https://github.com/Fleuronic/Metric", branch: "main")],
+	targets: [
+		.target(
+			name: "Parametric",
+			dependencies: ["Metric"]
+		),
+		.testTarget(
+			name: "ParametricTests",
+			dependencies: ["Parametric"]
+		)
+	]
+)
